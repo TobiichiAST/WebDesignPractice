af113028fc5b1bcda275e0a17020433e955ddf00
Tobiichi Origami <junhuih@andrew.cmu.edu>
2018-06-24 16:23:55 +0800

    better nav bar; main screen and toshow effect; add a thanks page

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+//$(document).ready means when the page loads
+$(document).ready(function () {
+	$('.toshow h1').delay(100).animate({'opacity':'1'},1200);
+	$('.toshow p').delay(600).animate({'opacity':'1'},1200);
+});

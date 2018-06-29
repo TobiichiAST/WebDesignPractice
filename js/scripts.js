5afe0fef56ff33091c8cc720e79b01e548b95cb3
Tobiichi Origami <junhuih@andrew.cmu.edu>
2018-06-29 11:09:28 +0800

    add a sample loading screen

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
+
+$(document).ready(function() {
+    setTimeout(function(){
+        $('body').addClass('loaded');
+    }, 500);
+    setTimeout(function(){
+  		$('#loader-wrapper').css({ zIndex: '0' });
+}, 500);
+ 
+});
+
 //$(document).ready means when the page loads
 $(document).ready(function () {
-	$('.toshow h1').delay(100).animate({'opacity':'1'},1200);
-	$('.toshow p').delay(600).animate({'opacity':'1'},1200);
+	$('.toshow h1').delay(800).animate({'opacity':'1'},1200);
+	$('.toshow p').delay(1200).animate({'opacity':'1'},1200);
 });

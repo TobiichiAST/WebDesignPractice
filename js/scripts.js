87d83cace8af2646c11d8f3fa8d827f5f4edfcbe
Tobiichi Origami <junhuih@andrew.cmu.edu>
2018-07-06 17:45:10 +0800

    a way more better navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 
 $(document).ready(function() {
     setTimeout(function(){
-        $('body').addClass('loaded');
-    }, 500);
+		$('body').addClass('loaded');
+	}, 500);
     setTimeout(function(){
-  		$('#loader-wrapper').css({ zIndex: '0' });
-}, 500);
+		$('#loader-wrapper').css({ zIndex: '0' });
+	}, 500);
+	setTimeout(function(){
+		$('#loader-wrapper').css({ display: 'none' });
+	}, 800);
  
 });
 
@@ -14,3 +17,20 @@ $(document).ready(function () {
 	$('.toshow h1').delay(800).animate({'opacity':'1'},1200);
 	$('.toshow p').delay(1200).animate({'opacity':'1'},1200);
 });
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    $('.topnav-background-transform').toggleClass('topnav-background-transform-active');
+    if (x.className == "topnav") {
+        x.className += " responsive";
+        $(".topnav.responsive a:not(:first-child)").each(function(i) {
+		    $(this).delay(100 * i).css('display', 'inline-block').hide().fadeIn(200);
+		});
+
+    } else {
+        x.className = "topnav";
+        $(".topnav a:not(:first-child):not(:last)").each(function(i) {
+		    $(this).delay(100 * i).fadeOut(200);
+		});
+    }
+}
